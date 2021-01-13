b6872ff8cce33fbc8e5d023caabe7972e2f7b7bf
kimjiwonpg98 <jiwonpg98@gmail.com>
2021-01-13 19:33:50 +0900

    Refactor: 필요한 값만 가져오기

--- routes/route.ctrl.js
+++ routes/route.ctrl.js
@@ -4,7 +4,13 @@ const db = require("../dbConfig/dbConfig");
 
 const output = {
   home: (req, res, next) => {
-    const sql = `SELECT * FROM boards`;
+    const sql = `SELECT board_no, 
+    student_no, 
+    board_title,
+    board_hit,
+    date_format(board_in_date, '%Y.%m.%d %H:%i') board_in_date,
+    date_format(board_update_date, '%Y.%m.%d %H:%i') board_update_date 
+    FROM boards`;
     db.query(sql, (err, rows) => {
       res.render("./home.ejs", { title: "게시판 리스트", rows: rows });
     });
